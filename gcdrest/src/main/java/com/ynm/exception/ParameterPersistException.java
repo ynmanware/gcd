58afd61b55c9e2a85bda8bc0fc953e6379b6b484
Yogesh Manware <ynmanware@gmail.com>
2017-06-27 19:10:07 +1000

    - committing missing files

---
+++ gcdrest/src/main/java/com/ynm/exception/ParameterPersistException.java
@@ -0,0 +1,16 @@
+package com.ynm.exception;
+
+/**
+ * @author Yogesh.Manware
+ *
+ */
+public class ParameterPersistException extends RuntimeException {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -4500839257641928450L;
+
+	public ParameterPersistException(String message) {
+		super(message);
+	}
+}
